c53eb7d55b0f959d9960dc7890054dfc52a5225a
Enrico Toniato <2827496+tonxxd@users.noreply.github.com>
2025-01-05 17:15:11 +0100

    add ads script

--- web-app/pages/_document.js
+++ web-app/pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
@@ -24,25 +24,48 @@ export default class MyDocument extends Document {
           />
 
           <title>Images to Video Convertor</title>
-          
 
-          <meta name="description" content="convert images for free of type .png .jpeg .exr to .mp4"/>
+          <meta
+            name="description"
+            content="convert images for free of type .png .jpeg .exr to .mp4"
+          />
 
-          <meta name="keywords" content="exr, images, jpeg, ,jpg ,png, convert, mp4"/>
-          <meta name="author" content="Etienne Salimbeni"/>
-
+          <meta
+            name="keywords"
+            content="exr, images, jpeg, ,jpg ,png, convert, mp4"
+          />
+          <meta name="author" content="Etienne Salimbeni" />
 
           <link rel="shortcut icon" href="/favicon_io/favicon.ico" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png"/>
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png"/>
-
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon_io/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon_io/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon_io/favicon-16x16.png"
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
+
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2842973901293778"
+            crossorigin="anonymous"
+          ></script>
         </body>
       </Html>
-    )
+    );
   }
 }
